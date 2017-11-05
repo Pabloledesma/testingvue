43dd37e483d2504466777d36f00a5b467473c7f0
PabloLedesma <pabloledes83@gmail.com>
2017-11-05 08:01:53 -0500

    episode 1

---
+++ test/counter.spec.js
@@ -0,0 +1,28 @@
+import { mount } from 'vue-test-utils';
+import Counter from '../src/components/Counter';
+import expect from 'expect';
+
+describe('Counter', () => {
+
+    let wraper;
+    beforeEach(() => {
+        wraper = mount(Counter);
+    });
+
+    it('defaults to a count of 0', () => {
+        expect(wraper.vm.count).toBe(0);
+    });
+    
+    it('increments the count when the button is clicked', () => {
+        expect(wraper.vm.count).toBe(0);
+        wraper.find('button').trigger('click');
+        expect(wraper.vm.count).toBe(1);
+    });
+    
+    it('presents the current count', () => {
+        expect(wraper.find('.count').html()).toContain(0);
+        wraper.find('button').trigger('click');
+        expect(wraper.find('.count').html()).toContain(1);
+    });
+
+});
